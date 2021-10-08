8ad29427ff1f25a7db51e08296f2e6f1c97b0b3f
erie0210 <eriee0210@gmail.com>
2021-10-08 23:53:49 +0900

    test checkpoint: bakery calculator, comments

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,14 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { Bakery } from 'src/bakery/bakery.schema';
+import { BakeryRepository } from 'src/bakery/bakery.repository';
+import { Comments } from './comments.schema';
 import { CommentsController } from './comments.controller';
+import { CommentsRepository } from './comments.repository';
+import { CommentsService } from './comments.service';
+import { Recipe } from 'src/recipe/recipe.schema';
+import { RecipeRepository } from 'src/recipe/recipe.repository';
+import { Users } from 'src/users/users.schema';
+import { UsersRepository } from 'src/users/users.repository';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('CommentsController', () => {
   let controller: CommentsController;
+  let service: CommentsService;
+  let repository: CommentsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CommentsService,
+        CommentsRepository,
+        RecipeRepository,
+        UsersRepository,
+        BakeryRepository,
+        {
+          provide: getModelToken(Comments.name),
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Recipe.name),
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Users.name),
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Bakery.name),
+          useFactory: () => {},
+        },
+      ],
       controllers: [CommentsController],
     }).compile();
 
+    repository = await module.get<CommentsRepository>(CommentsRepository);
+    service = await module.get<CommentsService>(CommentsService);
     controller = module.get<CommentsController>(CommentsController);
   });
 
--- src/comments/comments.service.spec.ts
+++ src/comments/comments.service.spec.ts
@@ -1,8 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { Bakery } from 'src/bakery/bakery.schema';
+import { BakeryRepository } from 'src/bakery/bakery.repository';
 import { Comments } from './comments.schema';
 import { CommentsRepository } from './comments.repository';
 import { CommentsService } from './comments.service';
+import { Recipe } from 'src/recipe/recipe.schema';
+import { RecipeRepository } from 'src/recipe/recipe.repository';
+import { Users } from 'src/users/users.schema';
+import { UsersRepository } from 'src/users/users.repository';
 import { getModelToken } from '@nestjs/mongoose';
 
 describe('CommentsService', () => {
@@ -14,8 +20,23 @@ describe('CommentsService', () => {
       providers: [
         CommentsService,
         CommentsRepository,
+        RecipeRepository,
+        UsersRepository,
+        BakeryRepository,
         {
           provide: getModelToken(Comments.name),
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Recipe.name),
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Users.name),
+          useFactory: () => {},
+        },
+        {
+          provide: getModelToken(Bakery.name),
           useFactory: () => {},
         },
       ],
